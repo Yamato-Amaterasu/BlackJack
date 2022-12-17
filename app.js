7d55843b5f7832cf8930290a07dde387f882903c
Mohamad Ayyad <hamoodaayyad@gmail.com>
2022-12-17 14:24:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+////////// THE RULES SO FAR \\\\\\\\\\
+// 1. the goal is to have a higher total than the other players but not over 21
+// 2. if higher than 21 then you "bust" and lose
+// 3. the game starts with the players placing bets
+// 4. then the dealer deals one card facing up to each player then to themselves(the dealer)
+// 5. everyone is delt one more more card facing up besides the dealer
+// 5.5 so the dealer has 1 card facing up and the 2nd one not showing while the other players have 2 cards facing up
+// 6. cards 2 through 10 have the value of their number ( so 2 is 2 points etc)
+// 7. J Q K are worth 10 points. the ace can be either 1 or 11 points you can change it in the middle of the round
+// 8. if your first 2 cards are worth 21, then you win 1.5 times your bet from the dealer and you are done for the rest of the game
+// 9. theres no limit on how many cards you can have.  you can say "hit " to get another card or say "stay " to not get a card
+// 10. once the dealer has gone over all the player and circle back to said dealer, the dealer shows their 2nd card thats not flipped
+// 11. if the dealers total is 16 or under they must take another card. if its 17 or higher they must stay with their cards
+// 12. if the dealer busts all players win  twice their bet. if the dealer doesnt bust, only the player that is higher than the dealer wins twice their bet. all other players lose their bet.\
+// 13. thats the end of one round so another rounds starts and we start over from step 3
